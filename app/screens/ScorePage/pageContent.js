31da94f4c82c5bace9a8570eb833e869d72380a0
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2025-10-28 14:31:57 -0600

    Update pageContent.js

--- app/screens/ScorePage/pageContent.js
+++ app/screens/ScorePage/pageContent.js
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import { useSearchParams } from 'next/navigation';
-import {Suspense} from 'react'
 
 export default function ScorePageContent() {
   const searchParams = useSearchParams();
