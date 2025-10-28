881fc70cbd7f8f8be7f1759761c50d59623d692f
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2025-10-28 14:29:43 -0600

    Fixed score page

--- app/screens/ScorePage/pageContent.js
+++ app/screens/ScorePage/pageContent.js
@@ -1,12 +1,16 @@
 'use client';
 
+import React from 'react';
 import { useSearchParams } from 'next/navigation';
+import {Suspense} from 'react'
 
 export default function ScorePageContent() {
   const searchParams = useSearchParams();
   const score = searchParams.get('score');
 
   return (
-    <p className="text-3xl pb-2">{score}</p>
+        <Suspense fallback={<div>Loading...</div>}>
+            <p className='text-3xl pb-2'>{score}</p>
+        </Suspense>
   );
 }
