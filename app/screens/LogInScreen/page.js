4ecc2e284cd60faa4fe911f3ebab48dfb900d0fd
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2025-10-29 10:30:18 -0600

    Fixed android log-in page

--- app/screens/LogInScreen/page.js
+++ app/screens/LogInScreen/page.js
@@ -63,75 +63,143 @@ export default function LogInScreen() {
     }
 
     return (
-        <main className="flex flex-col bg-gray-600 text-black min-h-screen overflow-hidden">
-            {/*Header */}
-            <div className="justify-center items-center hidden lg:flex flex-col w-full">
-                <img className='pt-10 w-60' src='/assets/ThatIsMyGameBanner.gif' alt='My game banner'></img>
-            </div>
-
-            {/*Middle Section */}
-
-            {/*Card 1*/}
-            <div className='grow overflow-hidden flex flex-col lg:flex-row mb-10 mt-10 items-center justify-center space-y-5 lg:space-y-0 lg:space-x-10'>
-                <div className="flex flex-col lg:flex-row shadow-lg rounded-lg w-full max-w-lg">
-                    {/* Log In*/}
-                    {!showSignUp && (
-                        <div className='bg-purple-600  shadow-lg rounded-lg p-12 w-full max-w-lg text-center overflow-hidden'>
-                            <input
-                                className='text-3xl w-[90%] h-12 border bg-white border-gray-300 shadow-lg text-center  rounded-md mr-3 ml-3 mb-3'
-                                type="email" 
-                                value={email} 
-                                onChange={(e) => setEmail(e.target.value)} 
-                                placeholder="Email"  
-                            />
-                            <br/>
-                            <input 
-                                className="text-3xl w-[90%] h-12 border bg-white border-gray-300 shadow-lg text-center rounded-md mr-3 ml-3 mb-3"
-                                type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                placeholder="Password"
-                            />
-                            <br/>
-                            <button onClick={LoginHandler} className='border-none bg-transparent cursor-pointer'>
-                                <img src="/assets/LogInButt.gif" className='w-41'></img>
-                            </button>
-                        </div>
-                    )}
-                    {/*Sign Up*/}
-                    { showSignUp && (
-                        <div className='bg-purple-600  shadow-lg rounded-lg p-12 w-full max-w-lg text-center overflow-hidden'>
-                            <input
-                                className='text-3xl w-[90%] h-12 border bg-white border-gray-300 shadow-lg text-center  rounded-md mr-3 ml-3 mb-3'
-                                type='email'
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                placeholder='Email'
-                            />
-                            <input
-                                className='text-3xl w-[90%] h-12 border bg-white border-gray-300 shadow-lg text-center  rounded-md mr-3 ml-3 mb-3'
-                                type='password'
-                                value={password}
-                                onChange={(e)=> setPassword(e.target.value)}
-                                placeholder='Password'
-                            />
-                            <button onClick={SignHandler} className='border-none bg-transparent cursor-pointer'>
-                                <img src="/assets/SignUpButt.gif" className='w-41' />
-                            </button>
-                        </div>
-                    )}
+        <main className=" bg-gray-600 text-black min-h-screen overflow-hidden">
+            <div className='hidden lg:flex lg:flex-col'>
+                {/*Header */}
+                <div className="justify-center items-center hidden lg:flex flex-col w-full">
+                    <img className='pt-10 w-60' src='/assets/ThatIsMyGameBanner.gif' alt='My game banner'></img>
                 </div>
 
-                {/*Card 2*/}
-                <div className="flex flex-col lg:flex-row shadow-lg rounded-lg w-full max-w-lg overflow-hidden">
+                {/*Middle Section -- PC VIEW */}
+
+                {/*Card 1*/}
+                <div className='grow overflow-hidden flex flex-col lg:flex-row mb-10 mt-10 items-center justify-center space-y-5 lg:space-y-0 lg:space-x-10'>
+                    <div className="flex flex-col lg:flex-row shadow-lg rounded-lg w-full max-w-lg">
+                        {/* Log In*/}
+                        {!showSignUp && (
+                            <div className='bg-purple-600  shadow-lg rounded-lg p-12 w-full max-w-lg text-center overflow-hidden'>
+                                <input
+                                    className='text-3xl w-[90%] h-12 border bg-white border-gray-300 shadow-lg text-center  rounded-md mr-3 ml-3 mb-3'
+                                    type="email" 
+                                    value={email} 
+                                    onChange={(e) => setEmail(e.target.value)} 
+                                    placeholder="Email"  
+                                />
+                                <br/>
+                                <input 
+                                    className="text-3xl w-[90%] h-12 border bg-white border-gray-300 shadow-lg text-center rounded-md mr-3 ml-3 mb-3"
+                                    type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    placeholder="Password"
+                                />
+                                <br/>
+                                <button onClick={LoginHandler} className='border-none bg-transparent cursor-pointer'>
+                                    <img src="/assets/LogInButt.gif" className='w-41'></img>
+                                </button>
+                            </div>
+                        )}
+                        {/*Sign Up*/}
+                        { showSignUp && (
+                            <div className='bg-purple-600  shadow-lg rounded-lg p-12 w-full max-w-lg text-center overflow-hidden'>
+                                <input
+                                    className='text-3xl w-[90%] h-12 border bg-white border-gray-300 shadow-lg text-center  rounded-md mr-3 ml-3 mb-3'
+                                    type='email'
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder='Email'
+                                />
+                                <input
+                                    className='text-3xl w-[90%] h-12 border bg-white border-gray-300 shadow-lg text-center  rounded-md mr-3 ml-3 mb-3'
+                                    type='password'
+                                    value={password}
+                                    onChange={(e)=> setPassword(e.target.value)}
+                                    placeholder='Password'
+                                />
+                                <button onClick={SignHandler} className='border-none bg-transparent cursor-pointer'>
+                                    <img src="/assets/SignUpButt.gif" className='w-41' />
+                                </button>
+                            </div>
+                        )}
+                    </div>
+
+                        {/*Card 2*/}
+                    <div className="flex flex-col lg:flex-row shadow-lg rounded-lg w-full max-w-lg overflow-hidden">
+                        {/* Sign Up Button -- Play Button */}
+                        <div className='bg-purple-600  shadow-lg rounded-lg p-12 w-full max-w-lg text-center overflow-hidden'>
+                            <button onClick={LogButtHandler} className='border-none bg-transparent cursor-pointer'>
+                                <img src="/assets/LogInButt.gif" alt='Back To Log In' className='w-59' />
+                            </button>
+                            <button onClick={SignUpHandler} className='border-none bg-transparent cursor-pointer'>
+                                <img src="/assets/SignUpButt.gif" className='w-59' />
+                            </button>
+                            <br/>
+                            <button onClick={GuessHandler} className='border-none bg-transparent cursor-pointer'>
+                                <img src="/assets/PlayButt.gif" className='w-59' alt="Play Button" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/*Middle Section -- Mobile View */}
+            <div className='lg:hidden min-h-full flex grow flex-col bg-purple-600'>
+                {/* Log In*/}
+                {!showSignUp && (
+                    <div className=' bg-purple-600 p-12 w-full mt-5 h-full text-center'>
+                        <input
+                            className='text-3xl w-[90%] h-12 border bg-white border-gray-300 shadow-lg text-center  rounded-md mr-3 ml-3 mb-3'
+                            type="email" 
+                            value={email} 
+                            onChange={(e) => setEmail(e.target.value)} 
+                            placeholder="Email"  
+                        />
+                        <br/>
+                        <input 
+                            className="text-3xl w-[90%] h-12 border bg-white border-gray-300 shadow-lg text-center rounded-md mr-3 ml-3 mb-3"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Password"
+                        />
+                        <br/>
+                        <button onClick={LoginHandler} className='border-none bg-transparent cursor-pointer'>
+                            <img src="/assets/LogInButt.gif" className='w-41'></img>
+                        </button>
+                    </div>
+                )}
+                {/*Sign Up*/}
+                { showSignUp && (
+                    <div className=' bg-purple-600 p-12 w-full mt-5 h-full text-center'>
+                        <input
+                            className='text-3xl w-[90%] h-12 border bg-white border-gray-300 shadow-lg text-center  rounded-md mr-3 ml-3 mb-3'
+                            type='email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder='Email'
+                        />
+                        <input
+                            className='text-3xl w-[90%] h-12 border bg-white border-gray-300 shadow-lg text-center  rounded-md mr-3 ml-3 mb-3'
+                            type='password'
+                            value={password}
+                            onChange={(e)=> setPassword(e.target.value)}
+                            placeholder='Password'
+                        />
+                        <button onClick={SignHandler} className='border-none bg-transparent cursor-pointer'>
+                            <img src="/assets/SignUpButt.gif" className='w-41' />
+                        </button>
+                    </div>
+                )}
+                <div className="border-t-2 grow flex flex-col lg:flex-row mt-10 shadow-lg w-full max-w-lg overflow-hidden">
                     {/* Sign Up Button -- Play Button */}
-                    <div className='bg-purple-600  shadow-lg rounded-lg p-12 w-full max-w-lg text-center overflow-hidden'>
-                        <button onClick={LogButtHandler} className='border-none bg-transparent cursor-pointer'>
-                            <img src="/assets/LogInButt.gif" alt='Back To Log In' className='w-59' />
-                        </button>
-                        <button onClick={SignUpHandler} className='border-none bg-transparent cursor-pointer'>
-                            <img src="/assets/SignUpButt.gif" className='w-59' />
-                        </button>
+                    <div className='bg-purple-600 grow p-12 w-full text-center'>
+                    {showSignUp ? 
+                    (<button onClick={LogButtHandler} className='border-none bg-transparent cursor-pointer'>
+                        <img src="/assets/LogInButt.gif" alt='Back To Log In' className='w-59' />
+                    </button>): 
+                    (<button onClick={SignUpHandler} className='border-none bg-transparent cursor-pointer'>
+                        <img src="/assets/SignUpButt.gif" className='w-59' />
+                    </button>)}
                         <br/>
                         <button onClick={GuessHandler} className='border-none bg-transparent cursor-pointer'>
                             <img src="/assets/PlayButt.gif" className='w-59' alt="Play Button" />
@@ -145,7 +213,7 @@ export default function LogInScreen() {
                 <h1>
                     @Author: Lee Valera, Aileen Pearl, Kevin Yabut
                 </h1>
-            </div>  
+            </div> 
         </main>
     );
 }
