4cd7bc21d0faf8157fb4d7f696fa5caf8d4b81ca
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2025-11-21 15:24:02 -0700

    up

--- app/games/tic-tac-toe/page.js
+++ app/games/tic-tac-toe/page.js
@@ -54,7 +54,7 @@ export default function ticTacToe() {
             <button
                 disabled={loading || status !== "ongoing"}
                 onClick={() => cellClick(row,col)}
-                className="lg:w-48 lg:h-48 h-36 w-36 bg-blue-300 border-2 border-black"
+                className="lg:w-40 lg:h-40 h-20 w-20 bg-blue-300 border-2 border-black"
             >   
                 {board[row][col] === "X" && <img src='/assets/tictactoeassets/X.gif'/>}
                 {board[row][col] === "O" && <img src='/assets/tictactoeassets/O.gif'/>}
@@ -166,7 +166,7 @@ export default function ticTacToe() {
 
 
     return (
-        <main className="grow overflow-hidden flex flex-col bg-gray-600 text-black items-center">
+        <main className="overflow-hidden min-h-screen flex flex-col bg-gray-600 text-black items-center">
             {/* Header */}
             <div className="relative flex flex-row items-center justify-between text-white mb-4 h-18 w-full px-4">
                 <button className='z-10' onClick={backHome}>
@@ -175,7 +175,7 @@ export default function ticTacToe() {
                 <h1 className="lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2 ml-auto text-right lg:text-5xl text-2xl font-bold">Tic-Tac-Toe</h1>
             </div>
 
-            <div className=' lg:w-[60%] w-full bg-purple-600 rounded-lg shadow-lg border-8 text-center flex flex-col justify-center items-center'>
+            <div className='grow lg:w-[60%] w-full bg-purple-600 rounded-lg shadow-lg border-8 text-center flex flex-col justify-center items-center'>
                 <div className='flex flex-col justify-center  items-center'>
                     <img className='w-20' src="/assets/GameTimer.gif" alt="Game Timer" />
                     <p className='lg:text-7xl text-3xl'>
@@ -183,7 +183,7 @@ export default function ticTacToe() {
                     </p>
                 </div>
                 <div
-                    className="bg-white grid grid-cols-3 lg:justify-center lg:items-center justify-start items-start"
+                    className="bg-white grid grid-cols-3 m-10 lg:justify-center lg:items-center justify-start items-start"
                 >
                 {board.map((row, i) =>
                     row.map((_, j) => (
